1fe8c7c3fa2e753d183b69cfdecccae61e998cb7
Hannguyl870 <Hannguyl870@SDS-L-H8QLG63>
2023-11-20 11:20:34 -0500

    jkf

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,12 +29,71 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.p1scorelable = new System.Windows.Forms.Label();
+            this.p2scorelable = new System.Windows.Forms.Label();
+            this.winlable = new System.Windows.Forms.Label();
+            this.gamertimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // p1scorelable
+            // 
+            this.p1scorelable.AutoSize = true;
+            this.p1scorelable.ForeColor = System.Drawing.Color.Snow;
+            this.p1scorelable.Location = new System.Drawing.Point(338, 9);
+            this.p1scorelable.Name = "p1scorelable";
+            this.p1scorelable.Size = new System.Drawing.Size(14, 16);
+            this.p1scorelable.TabIndex = 0;
+            this.p1scorelable.Text = "0";
+            // 
+            // p2scorelable
+            // 
+            this.p2scorelable.AutoSize = true;
+            this.p2scorelable.ForeColor = System.Drawing.Color.Snow;
+            this.p2scorelable.Location = new System.Drawing.Point(437, 9);
+            this.p2scorelable.Name = "p2scorelable";
+            this.p2scorelable.Size = new System.Drawing.Size(14, 16);
+            this.p2scorelable.TabIndex = 1;
+            this.p2scorelable.Text = "0";
+            // 
+            // winlable
+            // 
+            this.winlable.ForeColor = System.Drawing.Color.Snow;
+            this.winlable.Location = new System.Drawing.Point(338, 152);
+            this.winlable.Name = "winlable";
+            this.winlable.Size = new System.Drawing.Size(115, 28);
+            this.winlable.TabIndex = 2;
+            // 
+            // gamertimer
+            // 
+            this.gamertimer.Enabled = true;
+            this.gamertimer.Interval = 20;
+            this.gamertimer.Tick += new System.EventHandler(this.gamertimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.winlable);
+            this.Controls.Add(this.p2scorelable);
+            this.Controls.Add(this.p1scorelable);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label p1scorelable;
+        private System.Windows.Forms.Label p2scorelable;
+        private System.Windows.Forms.Label winlable;
+        private System.Windows.Forms.Timer gamertimer;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -12,9 +12,83 @@ namespace pog
 {
     public partial class Form1 : Form
     {
+        Rectangle player1 = new Rectangle(10, 170, 10, 60);
+        Rectangle player2 = new Rectangle(580, 170, 10, 60);
+        Rectangle ball = new Rectangle(295, 195, 10, 10);
+
+        int player1Score = 0;
+        int player2Score = 0;
+
+        int playerSpeed = 4;
+        int ballXSpeed = -6;
+        int ballYSpeed = 6;
+
+        bool wDown = false;
+        bool sDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+
+        SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+            }
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+            }
+
+        }
+        private void gamertimer_Tick(object sender, EventArgs e)
+        {
+            //move ball 
+            ball.X += ballXSpeed;
+            ball.Y += ballYSpeed;
+
+            Refresh();
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(blueBrush, player1);
+            e.Graphics.FillRectangle(blueBrush, player2);
+            e.Graphics.FillRectangle(whiteBrush, ball);
+        }
+
+      
     }
 }
